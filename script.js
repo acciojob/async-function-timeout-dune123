21cbf8ee492f835c65ba4ed72b2d4a7f910dddad
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-13 11:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const submitBtn=document.getElementById('btn')
+
+const action=(val,time)=>{
+return new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+         resolve(val);
+    },time);
+})
+}
+
+submitBtn.addEventListener('click',async()=>{
+    const textValue=document.getElementById('text').value;
+    const delayTime=document.getElementById('delay').value;
+
+    const val=await action(textValue,delayTime);
+    
+    document.getElementById('output').innerText=val;
+})
+
